ce9d05c6bfbc9a61355ac2c75493ccf764ea8d2d
ches4117 <ches4117@gmail.com>
2021-10-05 23:37:16 +0800

    feat: add react router to auto dispatch history search

--- src/App/Search/index.js
+++ src/App/Search/index.js
@@ -1,21 +1,39 @@
-import React, { useContext } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from 'react';
 import { debounce } from 'lodash';
 import { Navbar } from 'react-bootstrap';
+import { useHistory, useLocation } from 'react-router-dom';
 import { GithubContext } from '../../context';
 
 function Search() {
   const [, dispatch] = useContext(GithubContext);
+  const history = useHistory();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  const locationWord = query.get('word');
+
   const handleSearchChange = debounce((e) => {
     e.preventDefault();
     dispatch({
       type: 'setSearch',
       payload: { search: e.target.value },
     });
+    history.replace(`?word=${e.target.value}`);
   }, 500);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({
+        type: 'setSearch',
+        payload: { search: locationWord },
+      });
+    });
+  }, []);
 
   return (
     <Navbar style={{ backgroundColor: '#fff', position: 'sticky' }} fixed="top">
       <input
+        defaultValue={locationWord}
         className="form-control form-control-lg form-control-borderless"
         type="search"
         placeholder="Please input to search GitHub repo"
--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,19 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Search from './Search';
 import RepoList from './RepoList';
 import { ProviderContext } from '../context/provider';
 
 export default function App() {
   return (
-    <ProviderContext>
-      <Container>
-        <Search />
-        <RepoList />
-      </Container>
-    </ProviderContext>
+    <Router>
+      <ProviderContext>
+        <Container>
+          <Search />
+          <RepoList />
+        </Container>
+      </ProviderContext>
+    </Router>
   );
 }
